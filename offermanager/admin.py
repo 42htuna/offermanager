cfc54dd2c875ef439ed4871e29b79e46e01afcbd
Hakan Tuna <64739024+42htuna@users.noreply.github.com>
2020-06-21 17:13:21 +0300

    Add files via upload

---
+++ offermanager/admin.py
@@ -0,0 +1,84 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from . models import Employee, Customer, Offer, OfferItem
+
+# Register your models here.
+class EmployeeInline(admin.TabularInline):
+    model = Employee
+    can_delete = False
+    verbose_name_plural = _('Employee')
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (EmployeeInline,)
+
+class CustomerAdmin(admin.ModelAdmin):
+    fieldsets = [(None,	{'fields': ('name',
+                                    'address',
+                                    'phone',
+                                    'email')}),]
+    list_display = ['name',
+                    'address',
+                    'phone',
+                    'email',
+                    'created_by',
+                    'created_at',
+                    'updated_by',
+                    'updated_at']
+    list_filter = ['created_by',
+                   'created_at',
+                   'updated_by',
+                   'updated_at',]
+    search_fields = ['name',
+                     'address',
+                     'phone',
+                     'email',]
+
+    def save_model(self, request, obj, *args, **kwargs):
+        if not obj.created_by.id:
+            obj.created_by=request.user
+        obj.updated_by=request.user
+        obj.save()
+
+class OfferItemInline(admin.TabularInline):
+    model = OfferItem
+    extra = 0
+
+class OfferAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['customer']}),
+                 (_('DATE INFORMATION'), {'fields': ['date'], 'classes': ['collapse']}),
+                 (_('TERMS OF OFFER'), {'fields': ['terms'], 'classes': ['collapse wide']}),
+                 (None, {'fields': ['status', 'active']}),]
+
+    inlines = [OfferItemInline]
+    list_display = ['id',
+                    'customer',
+                    'date',
+                    'status',
+                    'status_offer',
+                    'active',
+                    'created_by',
+                    'created_at',
+                    'updated_by',
+                    'updated_at']
+    list_filter = ['customer',
+                   'date',
+                   'status',
+                   'active',
+                   'created_by',
+                   'created_at',
+                   'updated_by',
+                   'updated_at',]
+    search_fields = ['id', 'customer__title', 'terms']
+
+    def save_model(self, request, obj, *args, **kwargs):
+        if not obj.created_by.id:
+            obj.created_by=request.user
+        obj.updated_by=request.user
+        obj.save()
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Offer, OfferAdmin)
