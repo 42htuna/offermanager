84f4f42b01616912a95735da8764131af9f88826
Hakan Tuna <64739024+42htuna@users.noreply.github.com>
2020-12-10 15:26:13 +0300

    Update settings.py

--- offermanager/settings.py
+++ offermanager/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [("attachments", os.path.join(BASE_DIR, 'attachments')),]
